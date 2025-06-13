4aad1e7334212857b9faa6048ce2709e3eece0cd
Kanike Veeresh <veereshlee1821@gmail.com>
2025-06-13 16:38:06 +0530

    Final commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n-vw)uz-cv!ss*5^q!6&)yu^uz_v7(1qu2e+i&1p%u(&v=er+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['BlogApp.onrender.com', 'localhost']
 
 
 # Application definition
